993ddec990171bffa2d5ab45a0c773d25b53b670
ReYueY1ng <107557918+ReYueY1ng@users.noreply.github.com>
2026-06-30 20:00:46 +0800

    feat(mini_events): add actor revive event handling

---
+++ mn2mc/mini/packetevents/actor_revive.py
@@ -0,0 +1,10 @@
+import mn2mc.mini.proto as proto
+from mn2mc.mini.player import MiniPlayer
+from mn2mc.mini.packet import MiniClientPacket, add_event
+
+
+async def on_recv(player: MiniPlayer, mcp: MiniClientPacket) -> None:
+    player.mcclient.send("client_command", {"actionId": 0})
+
+
+add_event(proto.common.ePBMsgCode.PB_ACTOR_REVIVE_CH, on_recv)
